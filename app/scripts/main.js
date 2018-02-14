fa2c8cd3837ffc33ceaeace2b85205591c3a892e
Jack Merrell <jack@forpeople.co.uk>
2018-02-14 14:41:53 +0000

    adding darksky api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        console.log(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  var url = "";
+
+  function showPosition(position) {
+    getLocation();
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    url = "https://api.darksky.net/forecast/cd42d74eedbcef6a8614c6142db0c3ed/" + lat +','+ long;
+    console.log(url);
+
+    $.get(url, function(data) {
+    var currentTemperature = data.currently.temperature;
+    console.log(data);
+    document.getElementById('darkSkyTemperature').innerHTML ='Temperature: ' + currentTemperature;
+
+
+});
+}
+
+showPosition();
+
+});
